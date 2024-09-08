124e3481e2c91678d49ee616d20258ac46449c5c
FSKhan19 <farhanshahidkhan93@gmail.com>
2024-09-08 22:33:34 +0500

    Update: Added API Request

--- DatingApp.UI/src/app/app.component.ts
+++ DatingApp.UI/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'DatingApp.UI';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+  apiBaseUrl = 'http://localhost:5233/api';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get(`${this.apiBaseUrl}/Users`).subscribe({
+      next: response => {
+        this.users = response;
+      },
+      error: error => {
+        console.log("error => ", error);
+      }
+    });
+  }
+
 }
