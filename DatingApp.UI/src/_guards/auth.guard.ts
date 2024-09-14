4a5f9bfbdd07440dbb2f2df2213313f8738525dd
FSKhan19 <farhanshahidkhan93@gmail.com>
2024-09-14 11:28:54 +0500

    Update: Bootswatch

--- DatingApp.UI/src/_guards/auth.guard.ts
+++ DatingApp.UI/src/_guards/auth.guard.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 export const authGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
   const toastr = inject(ToastrService);
-  console.log('authGuard Entered');
+
   return accountService.currentUser$.pipe(
     map((user) => {
       if (user) return true;
